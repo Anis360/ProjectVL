7f11a03099bc07f2445a838ef4e8b8c8d3963a70
malek <malek@veloxsolutions.ca>
2022-02-28 23:24:08 +0100

    fix edit (probably)

--- resources/js/components/AllProduct.vue
+++ resources/js/components/AllProduct.vue
@@ -1,79 +1,97 @@
 <template>
+  <section>
+    <h2 class="text-center">Products list</h2>
     <div>
-        <h2 class="text-center">Products list</h2>
-        <div>
-    <b-button @click="modalShow = !modalShow">Add</b-button>
-<input type="text"
-         placeholder="Filter"
-         v-model="filter" />
-    
-  </div>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Detail</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="product in filteredRows" :key="product.id">
-                    <td>{{product.id}}</td>
-                    <td>{{product.name}}</td>
-                    <td>{{product.detail}}</td>
-                    <td>
-                        <div class="btn-group" role="group">
-                            <router-link :to="{name: 'edit', params: {id: product.id}}" class="btn btn-success">Edit</router-link>
-                            <b-button :to="{name: 'edit', params: {id: product.id}}" class="btn btn-success" @click="modalShow1 = !modalShow1">Edit</b-button>
-                            <button class="btn btn-danger" @click="deleteProduct(product.id)">Delete</button>
-                        </div>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        <b-modal v-model="modalShow" ><CreateProduct /></b-modal>
-        <b-modal v-model="modalShow1" ><EditProduct /></b-modal>
+      <b-button @click="showAddModalComponent">Add</b-button>
+      <input type="text" placeholder="Filter" v-model="filter"/>
     </div>
+
+    <table class="table">
+      <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Detail</th>
+        <th>Actions</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="product in filteredRows" :key="product.id">
+        <td>{{product.id}}</td>
+        <td>{{product.name}}</td>
+        <td>{{product.detail}}</td>
+        <td>
+          <div class="btn-group" role="group">
+            <b-button class="btn btn-success" @click="showEditModalComponent(product.id)">
+              Edit
+            </b-button>
+            <button class="btn btn-danger" @click="deleteProduct(product.id)">Delete</button>
+          </div>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+    <b-modal v-model="showAddModal">
+      <CreateProduct/>
+    </b-modal>
+    <EditProduct :editProductId = "editProductId" v-model="showEditModal"/>
+  </section>
 </template>
 
 <script>
 import CreateProduct from './CreateProduct'
 import EditProduct from './EditProduct'
-    export default{
-        name: 'App',
+
+export default {
+  name: 'App',
   components: {
-    CreateProduct, EditProduct
+    CreateProduct,
+    EditProduct
   },
-       data(){
-           
-          return{products:[],filter:'', modalShow: false, modalShow1: false}
-        },
-       created(){
-            this.axios.get('http://localhost:8000/api/products/').then(response => {
-                this.products = response.data;
-             });
-        },
-       methods:{
-           deleteProduct(id){
-                this.axios.delete('http://localhost:8000/api/products/${id}').then(response =>{
-                    let i=this.products.map(data=>data.id).indexOf(id);
-                    this.products.splice(i, 1)
-                });
-            }
-        },
-        computed: {
-  filteredRows() {
-    return this.products.filter(product => {
-      const name = product.name.toString().toLowerCase();
-      const detail = product.detail.toString().toLowerCase();
-      const searchTerm = this.filter.toLowerCase();
 
-      return name.includes(searchTerm) ||
-        detail.includes(searchTerm);
-    });
+  data () {
+    return {
+      products: [],
+      filter: '',
+      editProductId: 0,
+      showAddModal: false,
+      showEditModal: false
+    }
+  },
+  created () {
+    this.axios.get('http://localhost:8000/api/products/').then(response => {
+      this.products = response.data
+    })
+  },
+
+  methods: {
+    deleteProduct (id) {
+      this.axios.delete('http://localhost:8000/api/products/${id}').then(response => {
+        // Todo replace this with an actual backend call (axios.get('url_for_all_products'))
+        let i = this.products.map(data => data.id).indexOf(id)
+        this.products.splice(i, 1)
+      })
+    },
+
+    showAddModalComponent () {
+      this.showAddModal = true;
+    },
+
+    showEditModalComponent (productId) {
+      this.showEditModal = true;
+      this.editProductId = productId
+    }
+  },
+
+  computed: {
+    filteredRows () {
+      return this.products.filter(product => {
+        const name = product.name.toString().toLowerCase()
+        const detail = product.detail.toString().toLowerCase()
+        const searchTerm = this.filter.toLowerCase()
+        return name.includes(searchTerm) || detail.includes(searchTerm)
+      })
+    }
   }
-},
-
-    } 
+}
 </script>
--- resources/js/components/EditProduct.vue
+++ resources/js/components/EditProduct.vue
@@ -1,40 +1,44 @@
 <template>
-    <div>
-        <h3 class="text-center">Edit Product</h3>
-        <div class="row">
-            <div class="col-md-6">
-                <form @submit.prevent="updateProduct">
-                    <div class="form-group">
-                        <label>Name</label>
-                        <input type="text" class="form-control" v-model="product.name">
-                    </div>
-                    <div class="form-group">
-                        <label>Detail</label>
-                        <input type="text" class="form-control" v-model="product.detail">
-                    </div>
-                    <br>
-                    <button type="submit" class="btn btn-primary">Update</button>
-                </form>
-            </div>
-        </div>
+  <div>
+    <h3 class="text-center">Edit Product</h3>
+    <div class="row">
+      <div class="col-md-6">
+        <form @submit.prevent="updateProduct">
+          <div class="form-group">
+            <label>Name</label>
+            <input type="text" class="form-control" v-model="product.name">
+          </div>
+          <div class="form-group">
+            <label>Detail</label>
+            <input type="text" class="form-control" v-model="product.detail">
+          </div>
+          <br>
+          <button type="submit" class="btn btn-primary">Update</button>
+        </form>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default{
-        props: ['product.id'],
-        data(){
-            return{product:{},}
-              },
-        created(){
-            this.axios.get(`http://localhost:8000/api/products/${this.$route.params.id}`).then((res)=> 
-                    {this.product = res.data;});
-                 },
-        methods:{
-            updateProduct(){
-                this.axios.patch(`http://localhost:8000/api/products/${this.$route.params.id}`, this.product).then((res)=> 
-                {this.$router.push({name: 'home'});});
-                           }
-                 }
-                } 
+export default {
+  props: {
+    editProductId
+  },
+  data () {
+    return {product: {}}
+  },
+  created () {
+    this.axios.get(`http://localhost:8000/api/products/${this.editProductId}`).then((res) => {
+      this.product = res.data
+    })
+  },
+  methods: {
+    updateProduct () {
+      this.axios.patch(`http://localhost:8000/api/products/${this.editProductId}`, this.product).then((res) => {
+        this.$router.push({name: 'home'})
+      })
+    }
+  }
+}
 </script>
